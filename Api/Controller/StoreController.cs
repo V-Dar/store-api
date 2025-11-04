31240930a37e58887a82e19bd4d4532d6a44bc4f
Victor Dar <victordar@mail.ru>
2025-11-04 15:54:50 +0300

    Добавлены библиотеки и первый контроллер

---
+++ Api/Controller/StoreController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controller
+{
+    [ApiController]
+    [Route("api/[controller]/[Action]")] //Преобразуется в api/Store/Get
+    public class StoreController : ControllerBase
+    {
+
+    }
+}
